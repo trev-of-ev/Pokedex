7454cffa72c23680c11d50a01a3e7c6022faa9f3
Trevor Guinn <tdguinn87@gmail.com>
2023-01-12 13:54:23 -0600

    STYLING THE POKEGAME

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -11,7 +11,9 @@ class Pokecard extends Component {
     return (
       <div className="Pokecard">
         <h1 className="Pokecard-title">{this.props.name}</h1>
-        <img src={imgSrc} alt="Charmander" />
+        <div className="Pokecard-image">
+          <img src={imgSrc} alt={this.props.name} />
+        </div>
         <div className="Pokecard-data">Type: {this.props.type}</div>
         <div className="Pokecard-data">EXP: {this.props.exp}</div>
       </div>
